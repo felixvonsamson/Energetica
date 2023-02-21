60b92f110b4ad21e809c1217e860f117486f91bb
Maximilien Tirard <maximilien.tirard@icloud.com>
2023-02-21 15:19:34 +0000

    Changed port to 5001
    Port 5000 is already in use on macOS, see https://stackoverflow.com/questions/69818376/localhost5000-unavailable-in-macos-v12-monterey

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from website import create_app
 socketio, app = create_app()
 
 if __name__ == "__main__":
-  socketio.run(app, debug=True, log_output=False, host="0.0.0.0")
+  socketio.run(app, debug=True, log_output=False, host="0.0.0.0", port=5001)
