26a751feeda21807a8e16ebecc4be3c28ac44a50
Felix <felixvonsamson@gmail.com>
2023-07-26 21:58:24 +0200

    adding ssl support

---
+++ main_ssl.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+
+from website import create_app
+
+socketio, app = create_app()
+
+if __name__ == "__main__":
+  socketio.run(app, 
+               debug=True, 
+               log_output=False, 
+               host="0.0.0.0", 
+               port=5443, 
+               keyfile="server_privatekey.pem", 
+               certfile="server_certificate.pem")
+               
