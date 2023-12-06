9a5091344de22fad08e29dcc61c34b6e229ece77
Felix <felixvonsamson@gmail.com>
2023-12-06 17:00:42 +0100

    reduced lvl 0 industry power consumption

--- website/config.py
+++ website/config.py
@@ -350,8 +350,8 @@ full_config = {
             "construction time": 1800,
             "construction energy": 20000000,
             "construction pollution": 1000,
-            "power consumption": 100000/1.4, # [W]
-            "income": 4000/1.35, # [¤/day]   --- TEMPORARY CHANGED FROM 2000 to 40000
+            "power consumption": 50000, # [W]
+            "income": 2000, # [¤/day]   --- TEMPORARY CHANGED FROM 2000 to 40000
             "price multiplier": 1.25,
             "power factor" : 1.4,
             "income factor" : 1.35,
