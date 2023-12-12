f08ee687723d0f2c178844f82a4e1e8cdc12c99b
Maximilien Tirard <maximilien.tirard@icloud.com>
2023-12-12 21:47:54 +0000

    Made flask-httpauth and flask-sock work together

--- website/rest_api.py
+++ website/rest_api.py
@@ -1,46 +1,62 @@
-from flask import Flask
-from flask import Blueprint, g, current_app, jsonify
+from flask import Blueprint, g, current_app
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import check_password_hash
-from .database import Player, Hex
+from .database import Player
 
 rest_api = Blueprint('rest_api', __name__)
 
-basic_auth = HTTPBasicAuth()
+def add_sock_handlers(sock, engine):
+    basic_auth = HTTPBasicAuth()
 
-@basic_auth.verify_password
-def verify_password(username, password):
-    player = Player.query.filter_by(username=username).first()
-    if player:
-        if check_password_hash(player.password, password):
-            print(f"{username} logged in via HTTP Basic")
-            return username
-        else:
-            print(f"{username} failed to log in via HTTP Basic")
+    @basic_auth.verify_password
+    def verify_password(username, password):
+        player = Player.query.filter_by(username=username).first()
+        if player:
+            if check_password_hash(player.password, password):
+                print(f"{username} logged in via HTTP Basic")
+                return username
+            else:
+                print(f"{username} failed to log in via HTTP Basic")
 
-@rest_api.before_request
-@basic_auth.login_required
-def check_user():
-    g.engine = current_app.config["engine"]
-    g.player = Player.query.filter_by(username=basic_auth.current_user()).first()
+    @rest_api.before_request
+    @basic_auth.login_required
+    def check_user():
+        g.engine = current_app.config["engine"]
+        g.player = Player.query.filter_by(username=basic_auth.current_user()).first()
 
-# Simply returns success, used on client-side to check auth before proceeding
-@rest_api.route("/rest_auth", methods=["GET"])
-def get_map():
-    return jsonify("Success")
+    @basic_auth.verify_password
+    def verify_password(username, password):
+        player = Player.query.filter_by(username=username).first()
+        if player:
+            if check_password_hash(player.password, password):
+                print(f"{username} logged in via HTTP Basic")
+                return username
+            else:
+                print(f"{username} failed to log in via HTTP Basic")
 
-# gets the map data from the database and returns it as a dictionary of arrays
-@rest_api.route("/rest_get_map", methods=["GET"])
-def rest_get_map():
-    hex_list = Hex.query.order_by(Hex.r, Hex.q).all()
-    response = {
-        "ids": [tile.id for tile in hex_list],
-        "solars": [tile.solar for tile in hex_list],
-        "winds": [tile.wind for tile in hex_list],
-        "hydros": [tile.hydro for tile in hex_list],
-        "coals": [tile.coal for tile in hex_list],
-        "oils": [tile.oil for tile in hex_list],
-        "gases": [tile.gas for tile in hex_list],
-        "uraniums": [tile.uranium for tile in hex_list]
-    }
-    return jsonify(response)
+    # Main WebSocket endpoint for Swift client
+    @sock.route("/rest_ws", bp = rest_api)
+    def rest_ws(ws):
+        print(f"Received WS connection from {g.player}")
+        # print(f"Received WS connection from ???")
+        ws.send("Hello there!")
+        while True:
+            data = ws.receive()
+            print(f"received on websocket: data = {data}")
+            ws.send(data)
+    
+    # # gets the map data from the database and returns it as a dictionary of arrays
+    # @rest_api.route("/rest_get_map", methods=["GET"])
+    # def rest_get_map():
+    #     hex_list = Hex.query.order_by(Hex.r, Hex.q).all()
+    #     response = {
+    #         "ids": [tile.id for tile in hex_list],
+    #         "solars": [tile.solar for tile in hex_list],
+    #         "winds": [tile.wind for tile in hex_list],
+    #         "hydros": [tile.hydro for tile in hex_list],
+    #         "coals": [tile.coal for tile in hex_list],
+    #         "oils": [tile.oil for tile in hex_list],
+    #         "gases": [tile.gas for tile in hex_list],
+    #         "uraniums": [tile.uranium for tile in hex_list]
+    #     }
+    #     return jsonify(response)
