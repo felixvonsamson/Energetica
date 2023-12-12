40bba700fa9dde3ae4d309ffd322652a2116e21b
Maximilien Tirard <maximilien.tirard@icloud.com>
2023-12-12 20:50:59 +0000

    First successful websocket connnection

--- website/sock_handlers.py
+++ website/sock_handlers.py
@@ -1,2 +1,15 @@
+import time
+
 def add_sock_handlers(sock, engine):
-    print("Adding WebSocket handlers")
+    # flask-sock test
+    @sock.route("/rest_ws")
+    def rest_ws(ws):
+        print("Received WS connection!")
+        print(f"ws object: {ws}")
+        time.sleep(0.3)
+        ws.send("Hello there!")
+        print("send websocket data")
+        while True:
+            data = ws.receive()
+            print(f"received on websocket: data = {data}")
+            ws.send(data)
