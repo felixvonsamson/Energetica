7eaacc51cc4cfb67d5b51ffee58c7001793a1d24
Maximilien Tirard <maximilien.tirard@icloud.com>
2024-01-16 21:49:06 +0000

    Linting for network_logic.js

--- website/static/network/network_logic.js
+++ website/static/network/network_logic.js
@@ -4,20 +4,20 @@ fetch("/get_networks") // retrieves list of all networks using api.py
   .then((response) => response.json())
   .then((data) => {
     sortedNetworks = data.sort();
-    if(sortedNetworks.length == 0){
-        var warning = document.getElementById("warning");
-        warning.innerHTML = '<div class="medium toast error txt_center margin" \
+    if (sortedNetworks.length == 0) {
+      var warning = document.getElementById("warning");
+      warning.innerHTML = '<div class="medium toast error txt_center margin" \
         style="padding:8px 16px">No network has been created yet, \
         please create one.</div>';
-        var joinNetworkForm = document.getElementById("join_network_form");
-        joinNetworkForm.style.display = "none";
+      var joinNetworkForm = document.getElementById("join_network_form");
+      joinNetworkForm.style.display = "none";
     }
     var selectElement = document.getElementById("choose_network");
-    for(network of sortedNetworks){
-        var option = document.createElement("option");
-        option.value = network;
-        option.text = network;
-        selectElement.appendChild(option);
+    for (network of sortedNetworks) {
+      var option = document.createElement("option");
+      option.value = network;
+      option.text = network;
+      selectElement.appendChild(option);
     }
   })
   .catch((error) => {
@@ -26,7 +26,7 @@ fetch("/get_networks") // retrieves list of all networks using api.py
   });
 
 let sortedNames;
-let invitations=[];
+let invitations = [];
 
 fetch("/get_usernames") // retrieves list of all names using api.py
   .then((response) => response.json())
@@ -37,15 +37,15 @@ fetch("/get_usernames") // retrieves list of all names using api.py
     console.log(error);
     console.error("Error:", error);
   });
-  
+
 let input = document.getElementById("invite_player");
-  
+
 input.addEventListener("keyup", (e) => {
   //Initially remove all elements (so if user erases a letter or adds new letter then clean previous outputs)
   removeElements();
   for (let i of sortedNames) {
     //convert input to lowercase and compare with each string
-    if (invitations.includes(i)){
+    if (invitations.includes(i)) {
       continue;
     }
     if (
@@ -68,7 +68,7 @@ function displayNames(value) {
   input.value = value;
   removeElements();
 }
-  
+
 function removeElements() {
   //clear all the item
   let items = document.querySelectorAll(".suggestions-items");
@@ -76,40 +76,40 @@ function removeElements() {
     item.remove();
   });
 }
-  
+
 function addPlayer() {
   let player = input.value;
   //add player to the goup members only if they actually exist
-  if(sortedNames.includes(player) && !invitations.includes(player)){
+  if (sortedNames.includes(player) && !invitations.includes(player)) {
     invitations.push(player);
     let groupMember = document.createElement("li");
     groupMember.classList.add("group_member");
     groupMember.style.cursor = "pointer";
-    groupMember.setAttribute("id","groupMember_" + player); //so that they can be removed afretwards
+    groupMember.setAttribute("id", "groupMember_" + player); //so that they can be removed afretwards
     groupMember.setAttribute("onclick", "removePlayer('" + player + "')");
     groupMember.innerHTML = player;
     document.querySelector(".invited_players").appendChild(groupMember);
     input.value = "";
-  }else{
+  } else {
     alert("This player is already in the list or doesn't exist");
   }
 }
-  
+
 function removePlayer(name) {
   //remove one of the group members
   invitations = invitations.filter(i => i != name);
   document.getElementById("groupMember_" + name).remove();
 }
-  
+
 function createNetwork() {
   let name = document.getElementById("network_name").value;
-  if (name.length < 3 || name.length > 40){
+  if (name.length < 3 || name.length > 40) {
     alert("The network name has to have between 3 and 40 characters");
     return;
   }
   socket.emit('create_network', name, invitations);
 }
 
-function leave_network(){
+function leave_network() {
   socket.emit('leave_network');
 }
