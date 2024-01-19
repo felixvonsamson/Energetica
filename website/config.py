felixvonsamson/Energetica

commit 05fff7802c485edffe0d265e85d6c6d644de9167

changing tile and player relationship to one to one (no list)

website/config.py
@@ -592,14 +592,14 @@ class Config(object):
     # update mining speeds according to reserves depleation
     def update_resource_extraction(config, player_id):
         player = Player.query.get(player_id)
-        if len(player.tile) == 0:
+        if player.tile is None:
             return
         assets = config.for_player[player_id]["assets"]
         me_factor = assets["mineral_extraction"]["prod factor"] ** player.mineral_extraction
-        assets["coal_mine"]["amount produced"] = full_config["assets"]["coal_mine"]["amount produced"] * player.tile[0].coal * me_factor
-        assets["oil_field"]["amount produced"] = full_config["assets"]["oil_field"]["amount produced"] * player.tile[0].oil * me_factor
-        assets["gas_drilling_site"]["amount produced"] = full_config["assets"]["gas_drilling_site"]["amount produced"] * player.tile[0].gas * me_factor
-        assets["uranium_mine"]["amount produced"] = full_config["assets"]["uranium_mine"]["amount produced"] * player.tile[0].uranium * me_factor
+        assets["coal_mine"]["amount produced"] = full_config["assets"]["coal_mine"]["amount produced"] * player.tile.coal * me_factor
+        assets["oil_field"]["amount produced"] = full_config["assets"]["oil_field"]["amount produced"] * player.tile.oil * me_factor
+        assets["gas_drilling_site"]["amount produced"] = full_config["assets"]["gas_drilling_site"]["amount produced"] * player.tile.gas * me_factor
+        assets["uranium_mine"]["amount produced"] = full_config["assets"]["uranium_mine"]["amount produced"] * player.tile.uranium * me_factor
 
     # regularly update minimg productivity for all players (mineral depletion)
     def update_mining_productivity(config):

website/api.py
@@ -44,7 +44,7 @@ def get_map():
     if with_id is None:
         return jsonify(hex_list)
     else :
-        return jsonify(hex_list, current_user.tile[0].id)
+        return jsonify(hex_list, current_user.tile.id)
 
 # gets all the player usernames (except it's own) and returns it as a list :
 @api.route("/get_usernames", methods=["GET"])

website/database.py
@@ -27,7 +27,7 @@ class Hex(db.Model):
     oil = db.Column(db.Float)
     gas = db.Column(db.Float)
     uranium = db.Column(db.Float)
-    player_id = db.Column(db.Integer, db.ForeignKey("player.id"), default=None) # ID of the owner of the tile
+    player_id = db.Column(db.Integer, db.ForeignKey("player.id"), unique=True, nullable=True) # ID of the owner of the tile
 
     def __repr__(self):
         return f'<Tile {self.id} wind {self.wind}>'
@@ -77,7 +77,7 @@ class Player(db.Model, UserMixin):
     sid = None
 
     # Position :
-    tile = db.relationship("Hex", backref='player')
+    tile = db.relationship("Hex", uselist=False, backref='player')
     network_id = db.Column(db.Integer, db.ForeignKey("network.id"), default=None)
 
     # Chats :

website/production_update.py
@@ -34,7 +34,7 @@ def update_ressources(engine):
     # DO THIS AT THE END !
     players = Player.query.all()
     for player in players:
-        if len(player.tile) == 0:
+        if player.tile is None:
             continue
         assets = engine.config[player.id]["assets"]
         demand = engine.data["current_data"][player.username]["demand"]
@@ -50,7 +50,7 @@ def update_ressources(engine):
                 amount_produced = min(max_prod, max_warehouse)
                 setattr(player, ressource, getattr(player, ressource) + 
                         amount_produced)
-                setattr(player.tile[0], ressource, max(0, getattr(player.tile[0], 
+                setattr(player.tile, ressource, max(0, getattr(player.tile, 
                                                     ressource) - amount_produced))
                 production_factor = 0.2 + 0.8*amount_produced/max_prod
                 energy_demand = assets[facility]["power consumption"] * getattr(
@@ -62,7 +62,7 @@ def update_ressources(engine):
 
     db.session.commit()
 
-# function that updates the electricity generation and storage status for all players according to capacity and external factors (and trade)
+# function that updates the electricity generation and storage status for all players according to capacity and external factors
 def update_electricity(engine):
     t = engine.data["current_t"]
     networks = Network.query.all()
@@ -75,7 +75,7 @@ def update_electricity(engine):
         }
         # For each player in the network, calculate the demand and the minimal amount of electricity generation at time t
         for player in network.members:
-            if len(player.tile) == 0:
+            if player.tile is None:
                 continue
             init_storage(engine, player, t)
             total_demand = calculate_demand(engine, player, t)
@@ -90,7 +90,7 @@ def update_electricity(engine):
     players = Player.query.all()
     for player in players:
         current_data = engine.data["current_data"][player.username]
-        if len(player.tile) == 0:
+        if player.tile is None:
             continue
         # Production update for players that are not in a network
         if player.network == None:
@@ -524,7 +524,7 @@ def renewables_generation(engine, player, assets, generation, t):
                                 * assets[facility]["power generation"]
                                 * getattr(player, facility))
     #SOLAR
-    power_factor = engine.data["current_irradiation"][t]/875 * player.tile[0].solar # 875 W/m2 is the maximim irradiation in Zürich
+    power_factor = engine.data["current_irradiation"][t]/875 * player.tile.solar # 875 W/m2 is the maximim irradiation in Zürich
     for facility in ["CSP_solar", "PV_solar"]:
         generation[facility][t] = (power_factor
                                 * assets[facility]["power generation"]
@@ -539,7 +539,7 @@ def renewables_generation(engine, player, assets, generation, t):
 def interpolate_wind(engine, player, t):
     if engine.data["current_windspeed"][t] > 100:
         return 0
-    windspeed = engine.data["current_windspeed"][t] * pow(player.tile[0].wind, 0.5)
+    windspeed = engine.data["current_windspeed"][t] * pow(player.tile.wind, 0.5)
     i = math.floor(windspeed)
     f = windspeed-i
     pc = engine.wind_power_curve
@@ -592,7 +592,7 @@ def reduce_demand(engine, demand_type, player, demand, assets, t):
         q_resource = engine.data["current_data"][player.username]["ressources"][resource_name]
         takeback = q_resource[t]-q_resource[t-1]
         setattr(player, resource_name, getattr(player, resource_name)-takeback)
-        setattr(player.tile[0], resource_name, getattr(player.tile[0], resource_name)+takeback)
+        setattr(player.tile, resource_name, getattr(player.tile, resource_name)+takeback)
         q_resource[t] = getattr(player, resource_name)
         energy_demand = 0.2 * assets[demand_type]["power consumption"] * getattr(player, demand_type)
         demand[demand_type][t] = min(demand[demand_type][t-1]+0.2*energy_demand, energy_demand) # for smooth demand changes

website/rest_api.py
@@ -45,7 +45,7 @@ def add_sock_handlers(sock, engine):
         ws.send(rest_get_map())
         ws.send(rest_get_players())
         ws.send(rest_get_current_player(current_player=g.player))
-        if len(g.player.tile) != 0:
+        if g.player.tile is not None:
             rest_init_ws_post_location(ws)
         if g.player.id not in engine.websocket_dict:
             engine.websocket_dict[g.player.id] = []
@@ -97,7 +97,7 @@ def rest_get_players():
             {
                 "id": player.id,
                 "username": player.username,
-                "tile": player.tile[0].id if len(player.tile) > 0 else None,
+                "tile": player.tile.id if player.tile else None,
             }
             for player in player_list
         ],
@@ -261,7 +261,7 @@ def rest_confirm_location(engine, ws, data):
         existing_player = Player.query.get(location.player_id)
         ws.send(rest_server_alert_location_already_taken(existing_player))
         return
-    elif len(g.player.tile) != 0:
+    elif g.player.tile is not None:
         # Player already has a location
         # This is an invalid state - on the client side - so disconnect them
         ws.close()
@@ -283,7 +283,7 @@ def rest_add_player_location(player):
     JSON string."""
     response = {
         "type": "updatePlayerLocation",
-        "data": {"id": player.id, "tile": player.tile[0].id},
+        "data": {"id": player.id, "tile": player.tile.id},
     }
     return json.dumps(response)
 

website/socketio_handlers.py
@@ -27,7 +27,7 @@ def add_handlers(socketio, engine):
         location = Hex.query.get(id + 1)
         if location.player_id != None:
             current_user.emit("errorMessage", "Location already taken")
-        elif len(current_user.tile) != 0:
+        elif current_user.tile is not None:
             current_user.emit(
                 "errorMessage", "You already chose a location. Please refresh"
             )
@@ -77,7 +77,7 @@ def add_handlers(socketio, engine):
             return
         if facility in ["small_water_dam", "large_water_dam", "watermill"]:
             ud = Under_construction.query.filter_by(name=facility).count()
-            if current_user.tile[0].hydro <= getattr(current_user, facility) + ud:
+            if current_user.tile.hydro <= getattr(current_user, facility) + ud:
                 current_user.emit("errorMessage", "No suitable location available")
                 return
         if family in ["functional_facilities", "technologies"]:

website/templates/extraction_facilities.jinja
@@ -40,7 +40,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Coal_mining" target="_blank">{{facility["name"]}}</a></strong> &emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The coal mine extracts coal from the ground. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of coal : <strong><script>display_kg({{user.tile[0].coal}});</script></strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of coal : <strong><script>display_kg({{user.tile.coal}});</script></strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -81,7 +81,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Extraction_of_petroleum" target="_blank">{{facility["name"]}}</a></strong> &emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The oil field extracts oil from the ground. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of oil : <strong><script>display_kg({{user.tile[0].oil}});</script></strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of oil : <strong><script>display_kg({{user.tile.oil}});</script></strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -122,7 +122,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Natural_gas" target="_blank">{{facility["name"]}}</a></strong> &emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The gas drilling site extracts gas from the ground. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of gas : <strong><script>display_kg({{user.tile[0].gas}});</script></strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of gas : <strong><script>display_kg({{user.tile.gas}});</script></strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -163,7 +163,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Uranium_mining" target="_blank">{{facility["name"]}}</a></strong> &emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The uranium mine extracts uranium from the ground. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of uranium : <strong><script>display_kg({{user.tile[0].uranium}});</script></strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Underground reserves of uranium : <strong><script>display_kg({{user.tile.uranium}});</script></strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}

website/templates/power_facilities.jinja
@@ -90,7 +90,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Windmill" target="_blank">{{facility["name"]}}</a></strong>&emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The windmill uses the force of the wind to produce electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Wind potential : <strong>{{(user.tile[0].wind*100)|int}}%</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Wind potential : <strong>{{(user.tile.wind*100)|int}}%</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -131,7 +131,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Watermill" target="_blank">{{facility["name"]}}</a></strong>&emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The watermill uses the energy of flowing water to produce electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Remaining suitable locations : <strong>{{user.tile[0].hydro - user.watermill}}</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Remaining suitable locations : <strong>{{user.tile.hydro - user.watermill}}</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -298,7 +298,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Hydroelectricity" target="_blank">{{facility["name"]}}</a></strong> &emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The small water dam uses the potential energy of the water to produce electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Remaining suitable locations : <strong>{{user.tile[0].hydro - user.small_water_dam}}</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Remaining suitable locations : <strong>{{user.tile.hydro - user.small_water_dam}}</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -339,7 +339,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Wind_turbine" target="_blank">{{facility["name"]}}</a></strong>&emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The wind turbine uses the energy of the wind to produce electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Wind potential : <strong>{{(user.tile[0].wind*100)|int}}%</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Wind potential : <strong>{{(user.tile.wind*100)|int}}%</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -466,7 +466,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Hydroelectricity" target="_blank">{{facility["name"]}}</a></strong> &emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The large water dam uses the potential energy of the water to produce a large amount of electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Remaining suitable locations : <strong>{{user.tile[0].hydro - user.large_water_dam}}</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Remaining suitable locations : <strong>{{user.tile.hydro - user.large_water_dam}}</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -507,7 +507,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Concentrated_solar_power" target="_blank">{{facility["name"]}}</a></strong>&emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The concentrated solar power facility acumulates heat from the irradiation energy of the sun to produce electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Solar potential : <strong>{{(user.tile[0].solar*100)|int}}%</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Solar potential : <strong>{{(user.tile.solar*100)|int}}%</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -548,7 +548,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Photovoltaics" target="_blank">{{facility["name"]}}</a></strong>&emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The photovoltaic pannels transform the irradiation energy of the sun into electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Solar potential : <strong>{{(user.tile[0].solar*100)|int}}%</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Solar potential : <strong>{{(user.tile.solar*100)|int}}%</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}
@@ -589,7 +589,7 @@
  <div class="infotext padding">
   <strong class="large"><a class="no_deco" href="https://en.wikipedia.org/wiki/Wind_turbine" target="_blank">{{facility["name"]}}</a></strong>&emsp; <span class="money_font"><script>display_money_long({{facility["price"]}});</script></span><br>
   The wind turbine uses the energy of the wind to produce electricity. <br><br>
-  <em class="txt-blue"><i class="fa fa-info-circle"></i> Wind potential : <strong>{{(user.tile[0].wind*100)|int}}%</strong></em>
+  <em class="txt-blue"><i class="fa fa-info-circle"></i> Wind potential : <strong>{{(user.tile.wind*100)|int}}%</strong></em>
  </div>
  <div class="infobox" id="reqirements">
  {% if facility["locked"] %}

commit 0981dc22ad7e5ac9016649df7c7f9bec24ae862e

Fixed bug in update_resource_extraction
See changes introduced in 05fff7802c485edffe0d265e85d6c6d644de9167

website/config.py
@@ -747,22 +747,22 @@ class Config(object):
         )
         assets["coal_mine"]["amount produced"] = (
             full_config["assets"]["coal_mine"]["amount produced"]
-            * player.tile[0].coal
+            * player.tile.coal
             * me_factor
         )
         assets["oil_field"]["amount produced"] = (
             full_config["assets"]["oil_field"]["amount produced"]
-            * player.tile[0].oil
+            * player.tile.oil
             * me_factor
         )
         assets["gas_drilling_site"]["amount produced"] = (
             full_config["assets"]["gas_drilling_site"]["amount produced"]
-            * player.tile[0].gas
+            * player.tile.gas
             * me_factor
         )
         assets["uranium_mine"]["amount produced"] = (
             full_config["assets"]["uranium_mine"]["amount produced"]
-            * player.tile[0].uranium
+            * player.tile.uranium
             * me_factor
         )
 