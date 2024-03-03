f5cfa94a916d6b0b6e6e37a810b2d0ec270773b0
felixvonsamson <felixvonsamson@gmail.com>
2024-03-03 01:35:11 +0100

    facility tiles responsive (for smartphones)

--- website/static/click_construction.js
+++ website/static/click_construction.js
@@ -1,32 +1,34 @@
 function show_construction(name) {
     let tile = document.getElementById(name);
     let additionalContent = tile.querySelector("#constructionContent");
+    let additionalContent_smartphone = tile.querySelector("#constructionContent_smartphone");
     let infotable = tile.querySelector("#infotable");
     let overlay = tile.querySelector('.overlay');
+    let button = tile.querySelector('.construction_button_container');
 
     if (additionalContent.classList.contains("hidden")) {
         additionalContent.classList.remove("hidden");
         additionalContent.classList.add("shown");
-        additionalContent.style.maxHeight = null;
+
+        additionalContent_smartphone.classList.remove("hidden");
+        additionalContent_smartphone.classList.add("shown");
 
         infotable.classList.remove("minimized");
         infotable.classList.add("maximized");
-        infotable.style.maxHeight = "250 px";
 
         overlay.classList.add('hidden');
+        button.classList.remove('hidden');
     } else {
         additionalContent.classList.remove("shown");
         additionalContent.classList.add("hidden");
-        additionalContent.style.maxHeight = "100 px";
+
+        additionalContent_smartphone.classList.remove("shown");
+        additionalContent_smartphone.classList.add("hidden");
 
         infotable.classList.remove("maximized");
         infotable.classList.add("minimized");
-        infotable.style.maxHeight = "139 px";
 
         overlay.classList.remove('hidden');
+        button.classList.add('hidden');
     }
-}
-
-function stopPropagation(event) {
-    event.stopPropagation();
 }
