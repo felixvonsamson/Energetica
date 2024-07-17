bd1cd245314c93b4eb30d3fd2bb9deba8b51c08c
felixvonsamson <felixvonsamson@gmail.com>
2024-07-18 01:45:22 +0200

    removing obsolete O&M in databse_update

--- database_update.py
+++ database_update.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pickle
 from collections import deque
 
@@ -31,29 +32,41 @@ for network in networks:
             if "power" in player_capacities[facility]:
                 if facility not in old_engine_data["network_capacities"][network.id]._data:
                     old_engine_data["network_capacities"][network.id]._data[facility] = {"power": 0.0}
-                old_engine_data["network_capacities"][network.id]._data[facility]["power"] += player_capacities[facility]["power"]
-    
+                old_engine_data["network_capacities"][network.id]._data[facility]["power"] += player_capacities[
+                    facility
+                ]["power"]
+
     old_engine_data["network_data"][network.id]._data["consumption"] = {}
     old_engine_data["network_data"][network.id]._data["generation"] = {}
     for facility in old_engine_data["network_capacities"][network.id]._data:
         old_engine_data["network_data"][network.id]._data["consumption"][facility] = deque([0.0] * 360, maxlen=360)
         old_engine_data["network_data"][network.id]._data["generation"][facility] = deque([0.0] * 360, maxlen=360)
 
-    with open(f"instance/network_data/{network.id}/time_series.pck","rb") as file:
+    with open(f"instance/network_data/{network.id}/time_series.pck", "rb") as file:
         old_network_data = pickle.load(file)
-        for smth in old_network_data:
-            for smth2 in old_network_data[smth]:
         old_network_data["consumption"] = {}
         old_network_data["generation"] = {}
         for facility in old_engine_data["network_capacities"][network.id]._data:
             old_network_data["consumption"][facility] = [[0.0] * 360] * 5
             old_network_data["generation"][facility] = [[0.0] * 360] * 5
-        with open(f"instance/network_data/{network.id}/time_series.pck","wb") as file:
+        with open(f"instance/network_data/{network.id}/time_series.pck", "wb") as file:
             pickle.dump(old_network_data, file)
+
+# remove obselete O&M data
+players = session.query(Player).all()
+for player in players:
+    if Path(f"instance/player_data/player_{player.id}.pck").is_file():
+        with open(f"instance/player_data/player_{player.id}.pck", "rb") as file:
+            old_player_data = pickle.load(file)
+            if "O&M_costs" in old_player_data["revenues"]:
+                del old_player_data["revenues"]["O&M_costs"]
+        with open(f"instance/player_data/player_{player.id}.pck", "wb") as file:
+            pickle.dump(old_player_data, file)
+
 
 players = session.query(Player).all()
 # for player in players:
-    
+
 
 with open("instance/engine_data.pck", "wb") as new_file:
     pickle.dump(old_engine_data, new_file)
