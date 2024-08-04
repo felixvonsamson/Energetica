92a2eaf70b1c58bcab2e459902c76deb5a7c633a
felixvonsamson <felixvonsamson@gmail.com>
2024-08-04 23:27:09 +0200

    added check_climate_events() TODO: implement idividual event impacts

--- website/config.py
+++ website/config.py
@@ -1056,3 +1056,79 @@ class Config(object):
 
 
 config = Config()
+
+
+climate_events = {
+    "flood": {
+        "name": "Flood",
+        "base_probability": 0.001,  # [probability per tick]
+        "description": "A flood has occurred and destroyed some parts of the infrastructure. The recovery from this event will take {duration} days and cost {cost}.",
+        "cost_fraction": 0.6,  # fraction of the player's industry revenue that he has to pay
+        "duration": timedelta(days=6).total_seconds(),  # [in-game seconds]
+        "affected_tiles": 1,
+        "destruction_chance": {  # chance of destruction for each facility type
+            "watermill": 0.6,
+            "small_water_dam": 0.05,  # destruction affects the 3 downstream tiles
+            "large_water_dam": 0.01,  # destruction affects all downstream tiles
+            "industry": 0.5,  # level down by 1
+        },
+    },
+    "heat_wave": {
+        "name": "Heat wave",
+        "base_probability": 0.001,
+        "description": "A heat wave has occurred and is impacting the population. The recovery from this event will take {duration} days and cost {cost}.",
+        "cost_fraction": 0.5,
+        "duration": timedelta(days=3).total_seconds(),
+        "affected_tiles": 2,  # (a radius of 2 so 7 tiles)
+        "destruction_chance": {
+            "lithium_ion_batteries": 0.05,
+            "solid_state_batteries": 0.02,
+        },
+    },
+    "cold_wave": {
+        "name": "Cold wave",
+        "base_probability": 0.001,
+        "description": "A cold wave has occurred and is impacting the population. The recovery from this event will take {duration} days and cost {cost}.",
+        "cost_fraction": 0.5,
+        "duration": timedelta(days=3).total_seconds(),
+        "affected_tiles": 2,  # (a radius of 2 so 7 tiles)
+        "destruction_chance": {},
+    },
+    "hurricane": {
+        "name": "Hurricane",
+        "base_probability": 0.000_2,
+        "description": "A hurricane has occurred and destroyed some parts of the infrastructure. The recovery from this event will take {duration} days and cost {cost}.",
+        "cost_fraction": 0.8,
+        "duration": timedelta(days=9).total_seconds(),
+        "affected_tiles": 3,  # (a radius of 3 so 19 tiles)
+        "destruction_chance": {
+            "windmill": 0.4,
+            "onshore_wind_turbine": 0.2,
+            "offshore_wind_turbine": 0.1,
+            "PV_solar": 0.05,
+            "CSP_solar": 0.1,
+            "industry": 0.9,
+        },
+    },
+    "wildfire": {
+        "name": "Wildfire",
+        "base_probability": 0.000_7,
+        "description": "A wildfire has occurred and destroyed some parts of the infrastructure. The recovery from this event will take {duration} days and cost {cost}.",
+        "cost_fraction": 0.3,
+        "duration": timedelta(days=3).total_seconds(),
+        "affected_tiles": 2,  # (a radius of 2 so 7 tiles)
+        "destruction_chance": {
+            "windmill": 0.1,
+            "onshore_wind_turbine": 0.05,
+            "watermill": 0.05,
+            "steam_engine": 0.02,
+            "coal_burner": 0.02,
+            "oil_burner": 0.02,
+            "gas_burner": 0.02,
+            "combined_cycle": 0.02,
+            "PV_solar": 0.1,
+            "CSP_solar": 0.02,
+            "industry": 0.3,
+        },
+    },
+}
--- website/utils/game_engine.py
+++ website/utils/game_engine.py
@@ -2,18 +2,57 @@
 
 import json
 import pickle
+import random
 import time
+from calendar import c
 from datetime import datetime
+from sqlalchemy.sql import func
+import numpy as np
 
 import website.api.websocket as websocket
 import website.production_update as production_update
 import website.utils.assets as assets
 from website import db
-from website.database.engine_data import calculate_reference_gta, calculate_temperature_deviation
-from website.database.player_assets import ActiveFacilities, Shipment, UnderConstruction
+from website.config import climate_events
+from website.database.engine_data import (calculate_reference_gta,
+                                          calculate_temperature_deviation)
+from website.database.map import Hex
+from website.database.player_assets import (ActiveFacilities, Shipment,
+                                            UnderConstruction)
 from website.utils.assets import remove_asset
 from website.utils.misc import save_past_data_threaded
 from website.utils.resource_market import store_import
+
+
+def state_update(engine, app):
+    """This function is called every tick to update the state of the game"""
+    total_t = (time.time() - engine.data["start_date"]) / engine.clock_time
+    while engine.data["total_t"] < total_t - 1:
+        engine.data["total_t"] += 1
+        engine.log(f"t = {engine.data['total_t']}")
+        if engine.data["total_t"] % 216 == 0:
+            save_past_data_threaded(app, engine)
+        with app.app_context():
+            if engine.data["total_t"] % (600 / engine.clock_time) == 0:
+                engine.data["weather"].update_weather(engine)
+            log_entry = {
+                "timestamp": datetime.now().isoformat(),
+                "endpoint": "update_electricity",
+                "total_t": engine.data["total_t"],
+            }
+            engine.action_logger.info(json.dumps(log_entry))
+            production_update.update_electricity(engine=engine)
+            check_finished_constructions(engine)
+
+    # save engine every minute in case of server crash
+    if engine.data["total_t"] % (60 / engine.clock_time) == 0:
+        with open("instance/engine_data.pck", "wb") as file:
+            pickle.dump(engine.data, file)
+    with app.app_context():
+        # TODO: perhaps only run the below code conditionally on there being active ws connections
+        websocket.rest_notify_scoreboard(engine)
+        websocket.rest_notify_weather(engine)
+        websocket.rest_notify_global_data(engine)
 
 
 def check_finished_constructions(engine):
@@ -49,36 +88,84 @@ def check_finished_constructions(engine):
         db.session.commit()
 
 
-def state_update(engine, app):
-    """This function is called every tick to update the state of the game"""
-    total_t = (time.time() - engine.data["start_date"]) / engine.clock_time
-    while engine.data["total_t"] < total_t - 1:
-        engine.data["total_t"] += 1
-        engine.log(f"t = {engine.data['total_t']}")
-        if engine.data["total_t"] % 216 == 0:
-            save_past_data_threaded(app, engine)
-        with app.app_context():
-            if engine.data["total_t"] % (600 / engine.clock_time) == 0:
-                engine.data["weather"].update_weather(engine)
-            log_entry = {
-                "timestamp": datetime.now().isoformat(),
-                "endpoint": "update_electricity",
-                "total_t": engine.data["total_t"],
-            }
-            engine.action_logger.info(json.dumps(log_entry))
-            production_update.update_electricity(engine=engine)
-            check_finished_constructions(engine)
+def check_climate_events(engine):
+    """function that checks if a climate event happens on this tick"""
+    climate_change = engine.data["current_climate_data"].get_last_data()["temperature"]["deviation"]
+    ref_temp = engine.data["current_climate_data"].get_last_data()["temperature"]["reference"]
+    real_temp = climate_change + ref_temp
 
-    # save engine every minute in case of server crash
-    if engine.data["total_t"] % (60 / engine.clock_time) == 0:
-        with open("instance/engine_data.pck", "wb") as file:
-            pickle.dump(engine.data, file)
-    with app.app_context():
-        # TODO: perhaps only run the below code conditionally on there being active ws connections
-        websocket.rest_notify_scoreboard(engine)
-        websocket.rest_notify_weather(engine)
-        websocket.rest_notify_global_data(engine)
+    # floods
+    flood_probability = climate_events["flood"]["base_probability"] * climate_change**2
+    if random.random() < flood_probability:
+        while True:
+            random_tile_id = random.randint(1, Hex.query.count() + 1)
+            if Hex.query.get(random_tile_id).hydro > 0.2:
+                break
+        engine.log(f"Flood on tile {random_tile_id}")
+        tile = Hex.query.get(random_tile_id)
+        flood_impact(engine, tile)
 
+    # heatwaves
+    heatwave_probability = 0
+    if ref_temp > 15:
+        heatwave_probability += climate_events["heat_wave"]["base_probability"] * (ref_temp-15) * climate_change**2
+    if real_temp > 15:
+        heatwave_probability += climate_events["heat_wave"]["base_probability"] * (real_temp-15)**2
+    if random.random() < heatwave_probability:
+        random_latitude = max(-10, min(10, round(np.random.normal(0, 4))))
+        tile = Hex.query.filter(Hex.r == random_latitude).order_by(func.random()).first()
+        engine.log(f"Heatwave on tile {tile.id}")
+        heatwave_impact(engine, tile)
+
+    # coldwaves
+    coldwave_probability = 0
+    if ref_temp < 12.5:
+        coldwave_probability += climate_events["cold_wave"]["base_probability"] * (12.5-ref_temp) * climate_change**2
+    if real_temp < 12.5:
+        coldwave_probability += climate_events["cold_wave"]["base_probability"] * (12.5-real_temp)**2
+    if random.random() < coldwave_probability:
+        random_normal = max(-10, min(10, np.random.normal(0, 5)))
+        if random_normal < 0:
+            random_latitude = round(10 + random_normal)
+        else:
+            random_latitude = round(-10 + random_normal)
+        tile = Hex.query.filter(Hex.r == random_latitude).order_by(func.random()).first()
+        engine.log(f"Coldwave on tile {tile.id}")
+        coldwave_impact(engine, tile)
+
+    # hurricanes
+    hurricane_probability = climate_events["hurricane"]["base_probability"] * climate_change**2
+    if random.random() < hurricane_probability:
+        random_tile_id = random.randint(1, Hex.query.count() + 1)
+        tile = Hex.query.get(random_tile_id)
+        engine.log(f"Hurricane on tile {tile.id}")
+        hurricane_impact(engine, tile)
+
+    # wildfires
+    wildfire_probability = 0
+    if real_temp > 14:
+        wildfire_probability += climate_events["wildfire"]["base_probability"] * (real_temp-14)**2
+    if random.random() < wildfire_probability:
+        random_latitude = max(-10, min(10, round(np.random.normal(0, 5.5))))
+        tile = Hex.query.filter(Hex.r == random_latitude).order_by(func.random()).first()
+        engine.log(f"Wildfire on tile {tile.id}")
+        wildfire_impact(engine, tile)
+
+
+def flood_impact(engine, tile):
+    pass
+
+def heatwave_impact(engine, tile):
+    pass
+
+def coldwave_impact(engine, tile):
+    pass
+
+def hurricane_impact(engine, tile):
+    pass
+
+def wildfire_impact(engine, tile):
+    pass
 
 def data_init_climate(seconds_per_tick, random_seed, delta_t):
     """Initializes the data for the climate."""
@@ -105,4 +192,5 @@ def data_init_climate(seconds_per_tick, random_seed, delta_t):
             "reference": ref_temp,
             "deviation": temp_deviation,
         },
+    }
     }
