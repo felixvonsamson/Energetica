603a78516936801be71068ba3d3af132c28c0196
Yassir Akram <yakram@neuro3.inf.ethz.ch>
2024-11-18 23:54:04 +0100

    add: unit test that server can serve wiki

---
+++ tests/unit/test_server_runs.py
@@ -0,0 +1,28 @@
+import os
+import signal
+import subprocess
+from time import sleep
+
+import requests
+
+
+def test_server_runs():
+    process = subprocess.Popen(["python", "main.py", "--port", "5011"])
+
+    trials = 0
+    while True:
+        try:
+            response = requests.get("http://localhost:5011/wiki/introduction")
+        except requests.exceptions.ConnectionError:
+            trials += 1
+            if trials == 10:
+                assert False
+            sleep(1)
+        else:
+            assert response.status_code == 200
+            break
+    os.kill(process.pid, signal.SIGTERM)
+    process.wait()
+
+
+test_server_runs()
