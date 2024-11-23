57a7c2a73b06602d03c753bf1b925d121966787f
Maximilien Tirard <maximilien.tirard@icloud.com>
2024-11-23 22:48:17 +0100

    docs: add comment in game_engine and add type annotations

--- website/database/mixed_database.py
+++ website/database/mixed_database.py
@@ -2,6 +2,8 @@ from typing import Any, Callable
 
 from flask import current_app
 
+from website.game_engine import GameEngine
+
 
 def mixed_db(cls: object, data_fields: set[str], buffered_field: dict[str, Callable[[], Any]]):
     """
@@ -16,14 +18,14 @@ def mixed_db(cls: object, data_fields: set[str], buffered_field: dict[str, Calla
     """
 
     def __getattr__(self, name):
-        engine = current_app.config["engine"]
+        engine: GameEngine = current_app.config["engine"]
         if name in data_fields:
             return engine.data[cls.__name__][self.id][name]
         elif name in buffered_field:
-            buffered_value = engine.buffered_data[cls.__name__][self.id][name]
+            buffered_value = engine.buffered[cls.__name__][self.id][name]
             if buffered_value is None:
                 buffered_value = buffered_field[name]()
-                engine.buffered_data[cls.__name__][self.id][name] = buffered_value
+                engine.buffered[cls.__name__][self.id][name] = buffered_value
             return buffered_value
         elif name in self.__dict__:
             return self.__dict__[name]
@@ -33,11 +35,11 @@ def mixed_db(cls: object, data_fields: set[str], buffered_field: dict[str, Calla
     original_setattr = cls.__setattr__
 
     def __setattr__(self, name, value):
-        engine = current_app.config["engine"]
+        engine: GameEngine = current_app.config["engine"]
         if name in data_fields and self.id:
             engine.data[cls.__name__][self.id][name] = value
         elif name in buffered_field:
-            engine.buffered_data[cls.__name__][self.id][name] = value
+            engine.buffered[cls.__name__][self.id][name] = value
         else:
             original_setattr(self, name, value)
 
