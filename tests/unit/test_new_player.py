e4d4f086559998efcf2f3dd50689aa3d1d52fe21
Yassir Akram <yakram@neuro3.inf.ethz.ch>
2024-11-28 15:56:23 +0100

    minor change

--- tests/unit/test_new_player.py
+++ tests/unit/test_new_player.py
@@ -3,22 +3,23 @@ import sys
 
 sys.path.append(os.getcwd())
 
+from werkzeug.security import generate_password_hash
+
 from energetica import create_app
 from energetica.database import db
 from energetica.database.map import Hex
 from energetica.database.player import Player
 from energetica.utils.misc import confirm_location
-from werkzeug.security import generate_password_hash
 
 
 def test():
     _, app = create_app(rm_instance=True)
     app.app_context().push()
     engine = app.config["engine"]
-    player = Player(username="username100", pwhash=generate_password_hash("password"))
-    db.session.add(player)
-    db.session.commit()
-    hex_tile = db.session.get(Hex, 1)
-    confirm_location(engine, player, hex_tile)
-    db.session.commit()
-    assert True
+    with app.app_context():
+        player = Player(username="username", pwhash=generate_password_hash("password"))
+        db.session.add(player)
+        db.session.commit()
+        hex_tile = db.session.get(Hex, 1)
+        confirm_location(engine, player, hex_tile)
+        db.session.commit()
