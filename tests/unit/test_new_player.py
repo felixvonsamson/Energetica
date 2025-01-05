ae41472c8d9da585b78815be71bff8ee2cdb2a50
Yassir <yakram@inf.ethz.ch>
2025-01-05 12:16:41 +0100

    saving

--- tests/unit/test_new_player.py
+++ tests/unit/test_new_player.py
@@ -1,14 +1,8 @@
-import os
-import sys
-
-sys.path.append(os.getcwd())
-
 from werkzeug.security import generate_password_hash
 
 from energetica import create_app
 from energetica.database.map import HexTile
 from energetica.database.player import Player
-from energetica.globals import engine
 from energetica.utils.misc import confirm_location
 
 
