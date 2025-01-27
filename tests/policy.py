7ebc22e676fd3e9f7703f0ed4b6ffef47f9edfdd
Maximilien Tirard <maximilien.tirard@icloud.com>
2025-01-28 00:10:58 +0100

    feat: add starter policy and enhance policy runner with tick control

--- tests/policy.py
+++ tests/policy.py
@@ -2,8 +2,18 @@
 
 from __future__ import annotations
 
+import numpy as np
+
+from energetica import technology_effects
 from energetica.database.player import Player
-from energetica.enums import ControllableFacilityType, ProjectType
+from energetica.enums import (
+    ControllableFacilityType,
+    FunctionalFacilityType,
+    HydroFacilityType,
+    ProjectType,
+    WindFacilityType,
+    WorkerType,
+)
 from energetica.game_engine import Confirm, GameEngine
 from energetica.game_error import GameError
 from energetica.utils import assets
@@ -20,7 +30,7 @@ class Policy:
         self.name = name
         self.is_done = False
 
-    def take_action(self, _player: Player, _game_engine: GameEngine) -> None:
+    def take_action(self, _player: Player, _engine: GameEngine) -> None:
         """Every tick the policy will take an action based on the state"""
         self.is_done = True
 
@@ -51,13 +61,13 @@ class SequencedPolicy(Policy):
         super().__init__(", then ".join([policy.name for policy in policies]))
         self.policies = policies
 
-    def take_action(self, player: Player, game_engine: GameEngine) -> None:
+    def take_action(self, player: Player, engine: GameEngine) -> None:
         if self.is_done:
             return
         if not self.policies:
             self.is_done = True
             return
-        self.policies[0].take_action(player, game_engine)
+        self.policies[0].take_action(player, engine)
         if self.policies[0].is_done:
             self.policies.pop(0)
 
@@ -80,9 +90,9 @@ class ParallelPolicy(Policy):
         super().__init__(", then ".join([policy.name for policy in policies]))
         self.policies = policies
 
-    def take_action(self, player: Player, game_engine: GameEngine) -> None:
+    def take_action(self, player: Player, engine: GameEngine) -> None:
         for index in range(len(self.policies) - 1, -1, -1):
-            self.policies[index].take_action(player, game_engine)
+            self.policies[index].take_action(player, engine)
             if self.policies[index].is_done:
                 self.policies.pop(index)
         if not self.policies:
@@ -103,7 +113,7 @@ class WaitPolicy(Policy):
         super().__init__(f"wait {duration} ticks")
         self.duration = duration
 
-    def take_action(self, _player: Player, _game_engine: GameEngine) -> None:
+    def take_action(self, _player: Player, _engine: GameEngine) -> None:
         self.duration -= 1
         if self.duration <= 0:
             self.is_done = True
@@ -122,11 +132,107 @@ class QueueProjectPolicy(Policy):
         self.wait_for_funds = wait_for_funds
         self.wait_for_available_workers = wait_for_available_workers
 
-    def take_action(self, player: Player, _game_engine: GameEngine) -> None:
+    def take_action(self, player: Player, _engine: GameEngine) -> None:
         if self.is_done:
             return
         try:
             assets.queue_project(player, self.project_type)
+            print(f"player {player.id} queued project {self.project_type}")
             self.is_done = True
         except (GameError, Confirm):
             pass
+
+
+class StarterPolicy(Policy):
+    """
+    A policy adopts a strategy for the early game.
+
+    This should be the fastest / best strategy.
+    """
+
+    def __init__(self):
+        super().__init__("starter policy")
+
+    def take_action(self, player: Player, engine: GameEngine) -> None:
+        if self.is_done:
+            return
+
+        # Wait until all (until the) construction worker is free
+        if player.available_workers(WorkerType.CONSTRUCTION) < 1:
+            return
+
+        # Check if there is enough electric capacity to support an additional level of industry
+        current_industry = player.functional_facility_lvl[FunctionalFacilityType.INDUSTRY]
+        const_config_assets = engine.const_config["assets"]
+        next_industry_average_consumption = (
+            1.2
+            * const_config_assets["industry"]["base_power_consumption"]
+            * const_config_assets["industry"]["power_factor"] ** (current_industry + 1)
+        )
+        current_capacity = sum(gen["power"] for gen in player.capacities.get_all().values())
+        # print(
+        #     f"player {player.id} current capacity: {int(current_capacity / 1000)}KW next industry consumption: {int(next_industry_average_consumption / 1000)} kW"
+        # )
+        if next_industry_average_consumption < current_capacity:
+            try:
+                assets.queue_project(player, FunctionalFacilityType.INDUSTRY)
+                print(f"player {player.id} queued project {FunctionalFacilityType.INDUSTRY}")
+                return
+            except (GameError, Confirm):
+                # Not enough money. Wait for funds - return early
+                return
+
+        # Not enough power capacity, build a power facility. The strategy is:
+        # - Compute / estimate the LCOE for watermill, windmill, and steam engine
+        # - Build the cheapest one, or wait until funds are available
+        costs = {
+            facility_type: (
+                const_config_assets[facility_type]["base_price"]
+                * technology_effects.price_multiplier(player, facility_type)
+                * (
+                    technology_effects.hydro_price_multiplier(player, facility_type)
+                    if facility_type in HydroFacilityType
+                    else 1.0
+                )
+                * const_config_assets[facility_type]["O&M_factor_per_day"]
+                * 1.0
+                / (
+                    const_config_assets[facility_type]["base_power_generation"]
+                    * technology_effects.power_production_multiplier(player, facility_type)
+                )
+            )
+            for facility_type in (
+                ControllableFacilityType.STEAM_ENGINE,
+                HydroFacilityType.WATERMILL,
+                WindFacilityType.WINDMILL,
+            )
+        }
+        if WindFacilityType.WINDMILL in costs:
+            wind_speed_multiplier = technology_effects.wind_speed_multiplier(player, WindFacilityType.WINDMILL)
+
+            def capacity_factor_wind(wind_speed: float) -> float:
+                """Fit of empirical data for wind speeds."""
+                return 0.5280542813 - 0.5374832237 * np.exp(-2.226120465 * wind_speed**2.38728403)
+
+            # print(f"Capacity factor wind: {capacity_factor_wind(wind_speed_multiplier)}")
+            costs[WindFacilityType.WINDMILL] /= capacity_factor_wind(wind_speed_multiplier)
+
+        if HydroFacilityType.WATERMILL in costs:
+            costs[HydroFacilityType.WATERMILL] /= 0.55
+
+        # print(", ".join(f"{facility}: {1000000 * cost:.2f}" for facility, cost in costs.items()))
+        # find the cheapest facility from the dictionary costs
+        while costs:
+            cheapest_facility = min(costs, key=costs.get)
+            try:
+                assets.queue_project(player, cheapest_facility)
+                print(f"player {player.id} queued project {cheapest_facility}")
+                return
+            except Confirm:
+                # Not enough power! Try the next cheapest facility
+                costs.pop(cheapest_facility)
+                continue
+            except GameError:
+                # Not enough money. Wait for funds - return early
+                return
+            # The policy is done
--- tests/policy_runner.py
+++ tests/policy_runner.py
@@ -2,23 +2,32 @@
 
 from energetica.database.map import HexTile
 from energetica.database.player import Player
+from energetica.enums import Renewable
 from energetica.globals import engine
 from energetica.simulate import tick
 from energetica.utils import misc
 from tests.policy import Policy
 
 
-def run_policies(policies: list[Policy]) -> list[Player]:
+def run_policies(policies: list[Policy], ticks_to_run: int) -> list[Player]:
     """Run a list of policies with one player per policy"""
-    engine.init_instance(30, 240, 0)
+    engine.init_instance(30, 3600, 0)
     players = [Player(f"player_{i}", "password_hash") for i in range(len(policies))]
     for player in players:
-        misc.confirm_location(player, HexTile.getitem(player.id))
-    for tick_count in range(1, 100):
+        # tile = HexTile.getitem(player.id)
+        # tile = HexTile.getitem(53)
+        # tile = max(HexTile.all(), key=lambda t: t.potentials[Renewable.HYDRO])
+        # tile = max(HexTile.all(), key=lambda t: t.potentials[Renewable.WIND])
+        tile = max(HexTile.all(), key=lambda t: t.potentials[Renewable.WIND] * t.potentials[Renewable.HYDRO])
+        misc.confirm_location(player, tile)
+        print(f"Player {player.id} is at {player.tile}")
+        for renewable in Renewable:
+            print(f"{renewable}: {player.tile.potentials[renewable]}")
+    for tick_count in range(1, ticks_to_run + 1):
         for player, policy in zip(players, policies):
             policy.take_action(player, engine)
         tick()
-        print(f"Tick {tick_count}")
+        # print(f"Tick {tick_count}")
         if all(policy.is_done for policy in policies):
             print("All policies are done")
             break
