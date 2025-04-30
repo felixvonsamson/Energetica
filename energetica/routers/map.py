a7ac8f56ec82d1254e942b69d62b44e18f529346
mglst <maximilien.tirard@gmail.com>
2025-04-30 22:58:15 +0100

    refactor: update map API response structure and endpoints

--- energetica/routers/map.py
+++ energetica/routers/map.py
@@ -2,30 +2,32 @@ from __future__ import annotations
 
 from fastapi import APIRouter
 
+from energetica.schemas.map import HexTileOut
+
 router = APIRouter(prefix="/map", tags=["map"])
 
 
-@router.get("/get")
-def get_map() -> list[dict]:
-    """Get the map data from the database and returns it as a array of dictionaries."""
+@router.get("")
+def get_map() -> list[HexTileOut]:
+    """Get the map data."""
     from energetica.database.map import HexTile
     from energetica.enums import Fuel, Renewable
 
     hex_map = HexTile.all()
     hex_list = [
-        {
-            "id": tile.id,
-            "q": tile.coordinates[0],
-            "r": tile.coordinates[1],
-            "solar": tile.potentials[Renewable.SOLAR],
-            "wind": tile.potentials[Renewable.WIND],
-            "hydro": tile.potentials[Renewable.HYDRO],
-            "coal": tile.fuel_reserves[Fuel.COAL],
-            "gas": tile.fuel_reserves[Fuel.GAS],
-            "uranium": tile.fuel_reserves[Fuel.URANIUM],
-            "climate_risk": tile.climate_risk,
-            "player_id": tile.player.id if tile.player else None,
-        }
+        HexTileOut(
+            id=tile.id,
+            q=tile.coordinates[0],
+            r=tile.coordinates[1],
+            solar=tile.potentials[Renewable.SOLAR],
+            wind=tile.potentials[Renewable.WIND],
+            hydro=tile.potentials[Renewable.HYDRO],
+            coal=tile.fuel_reserves[Fuel.COAL],
+            gas=tile.fuel_reserves[Fuel.GAS],
+            uranium=tile.fuel_reserves[Fuel.URANIUM],
+            climate_risk=tile.climate_risk,
+            player_id=tile.player.id if tile.player else None,
+        )
         for tile in hex_map
     ]
     return hex_list
