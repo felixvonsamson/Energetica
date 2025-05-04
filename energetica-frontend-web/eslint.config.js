60db17cad0df537587ba1c84e9d94b1075bd7bfe
mglst <maximilien.tirard@gmail.com>
2025-05-04 11:17:02 +0100

    chore: setup React + Vite + TS

---
+++ energetica-frontend-web/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import reactDom from "eslint-plugin-react-dom";
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from "eslint-plugin-react-x";
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      react.configs.recommended,
+      reactDom.configs.recommended,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+);
---
+++ energetica-frontend-web/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <p>
+        Hello world!
+      </p>
+    </>
+  )
+}
+
+export default App
