58b1547c177af73181f647e17a3ee109873cdf91
mglst <maximilien.tirard@gmail.com>
2025-06-06 17:21:00 +0100

    Revert "refactor: migrated most flask api routes to fastapi routes"
    This reverts commit 91e9455b924a5162c292e7401faefdd570f0626b.

--- energetica/routers/__init__.py
+++ energetica/routers/__init__.py
@@ -27,6 +27,7 @@ from .players import router as player_router
 from .resource_market import router as resource_market_router
 from .templates import router as templates_router
 
+all_routers = [chat_router, daily_quiz_router, map_router, network_router, player_router, resource_market_router]
 __all__ = ["templates_router"]
 
 api_routers = [
