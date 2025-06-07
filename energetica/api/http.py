ec050b0e5573e8dbc00633dd0e9c177f96ca9f9b
mglst <maximilien.tirard@gmail.com>
2025-06-07 10:12:07 +0100

    fix: bug in request_delete_notification

--- energetica/api/http.py
+++ energetica/api/http.py
@@ -17,7 +17,7 @@ from energetica.auth import get_current_user
 from energetica.config.assets import wind_power_curve
 from energetica.database.active_facility import ActiveFacility
 from energetica.database.map import HexTile
-from energetica.database.messages import Chat
+from energetica.database.messages import Chat, Notification
 from energetica.database.network import Network
 from energetica.database.ongoing_project import OngoingProject
 from energetica.database.player import Player
@@ -53,8 +53,8 @@ async def request_delete_notification(  # noqa: ANN201
         }
     """
     request_data = await request.json()
-    notification_id = request_data["id"]
-    user.delete_notification(notification_id)
+    notification = Notification.getitem(int(request_data["id"]))
+    user.delete_notification(notification)
     return {"response": "success"}
 
 
