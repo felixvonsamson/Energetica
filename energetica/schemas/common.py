d21118d0dc850e196ad811de84c677b7353bcc82
mglst <maximilien.tirard@gmail.com>
2025-06-09 19:27:51 +0100

    chore: add todo for GameErrorResponse

--- energetica/schemas/common.py
+++ energetica/schemas/common.py
@@ -4,4 +4,5 @@ from pydantic import BaseModel
 class GameErrorResponse(BaseModel):
     """Response model for game errors."""
 
+    # TODO: rename from response to exception_type
     response: str
