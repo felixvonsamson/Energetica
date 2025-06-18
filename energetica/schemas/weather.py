dcf5220902b8ae2981f4801234eec0fbe845207f
mglst <maximilien.tirard@gmail.com>
2025-06-18 08:50:23 +0100

    docs: added descriptions to WeatherOut fields

--- energetica/schemas/weather.py
+++ energetica/schemas/weather.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel, Field
 
 
 class WeatherOut(BaseModel):
-    year_progress: float = Field(ge=0, le=1)
-    month_number: int = Field(ge=1, le=12)
-    solar_irradiance: float
-    wind_speed: float
-    river_discharge: float
+    year_progress: float = Field(ge=0, le=1, description="Fractional progression of the current year, from 0.0 to 1.0")
+    month_number: int = Field(ge=1, le=12, description="1=January, 2=February, ...")
+    solar_irradiance: float = Field(description="In units of TODO")
+    wind_speed: float = Field(description="In units of TODO")
+    river_discharge: float = Field(description="In units of TODO")
