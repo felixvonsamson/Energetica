55edac5fae68901481690877774ddf846d4a1bbd
mglst <maximilien.tirard@gmail.com>
2025-06-22 23:17:25 +0100

    docs: added comment about projects

--- energetica/schemas/projects.py
+++ energetica/schemas/projects.py
@@ -41,6 +41,8 @@ class ProjectOut(BaseApiModel):
 
 
 class ProjectsOut(BaseApiModel):
+    # TODO(mglst): it would make more sense for projects to be broken up into constructions_projects and
+    # research_projects. For example, when canceling a research project, we need to also fetch all constructions.
     projects: list[ProjectOut]
     construction_queue: list[int]
     research_queue: list[int]
