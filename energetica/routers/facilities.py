46ef1c457163b3829b720208e045306a8f6594c1
mglst <maximilien.tirard@gmail.com>
2025-06-24 22:54:09 +0100

    feat: new routes for upgrade & dismantle facilities

--- energetica/routers/facilities.py
+++ energetica/routers/facilities.py
@@ -1,5 +1,60 @@
 """Routes for facilities."""
 
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from energetica.auth import get_current_user
+from energetica.database.active_facility import ActiveFacility
+from energetica.database.player import Player
+from energetica.enums import ExtractionFacilityType, PowerFacilityType, StorageFacilityType
+from energetica.schemas.players import MoneyOut
+from energetica.utils import assets
 
 router = APIRouter(prefix="/facilities", tags=["Facilities"])
+
+
+@router.post("{facility_id}:upgrade")
+def upgrade_facility(
+    player: Annotated[Player, Depends(get_current_user)],
+    facility_id: int,
+) -> MoneyOut:
+    """Upgrade a facility."""
+    facility = ActiveFacility.getitem(facility_id, HTTPException(status_code=status.HTTP_404_NOT_FOUND))
+    if facility.player != player:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    assets.upgrade_facility(player=player, facility=facility)
+    return MoneyOut.from_player(player)
+
+
+@router.post(":upgrade-all")
+async def upgrade_all_of_type(
+    player: Annotated[Player, Depends(get_current_user)],
+    facility_type: PowerFacilityType | StorageFacilityType | ExtractionFacilityType,
+) -> MoneyOut:
+    """Upgrade all facilities of a certain type."""
+    assets.upgrade_all_of_type(player=player, facility_type=facility_type)
+    return MoneyOut.from_player(player)
+
+
+@router.post("{facility_id}:dismantle")
+async def dismantle_facility(
+    player: Annotated[Player, Depends(get_current_user)],
+    facility_id: int,
+) -> MoneyOut:
+    """Dismantle a facility."""
+    facility = ActiveFacility.getitem(facility_id, HTTPException(status_code=status.HTTP_404_NOT_FOUND))
+    if facility.player != player:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    assets.dismantle_facility(player=player, facility=facility)
+    return MoneyOut.from_player(player)
+
+
+@router.post(":dismantle-all")
+async def dismantle_all_of_type(
+    player: Annotated[Player, Depends(get_current_user)],
+    facility_type: PowerFacilityType | StorageFacilityType | ExtractionFacilityType,
+) -> MoneyOut:
+    """Dismantle all facilities of a certain type."""
+    assets.dismantle_all_of_type(player=player, facility_type=facility_type)
+    return MoneyOut.from_player(player)
