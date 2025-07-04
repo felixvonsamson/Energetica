f2794d095455d1c0ca2db0515bd5c1b1bc674f5b
mglst <maximilien.tirard@gmail.com>
2025-07-04 22:53:46 +0100

    chore: fix misc

--- energetica/routers/admin_actions.py
+++ energetica/routers/admin_actions.py
@@ -1,4 +1,4 @@
-"""roots for admin actions"""
+"""roots for admin actions."""
 
 from __future__ import annotations
 
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/admin-actions", tags=["Admin Actions"])
 
 # ensure the actions on this router are only accessible by admins
 @router.get("/chart-data/{player_id}")
-def chart_data(player_id: int):
+def chart_data(player_id: int):  # noqa: ANN201
     """Get chart data for the admin dashboard."""
-    player = Player.get(player_id)
+    player = Player.getitem(player_id)
     return get_chart_data(player)
