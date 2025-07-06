1293f14b5a5968557614653cb3ee5dc34315b785
felixvonsamson <felixvonsamson@gmail.com>
2025-07-06 15:35:41 +0200

    made the page responsive

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,33 +1,48 @@
 import { useState } from 'react';
 
 function NavBar() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <nav className="navbar medium">
-      <ul className="navbar-nav">
-        <NavBarItem name="Dashboard" icon="dashboard.png" />
-        <NavBarItem name="Profile" icon="profile.png" />
-        <NavBarSection name="Production Overview">
-          <NavBarItem name="Revenues" icon="revenues.png" />
-          <NavBarItem name="Electricity" icon="power_facilities.png" />
-          <NavBarItem name="Storage" icon="storage_facilities.png" />
-          <NavBarItem name="Resources" icon="resources.png" />
-          <NavBarItem name="Emissions" icon="emissions.png" />
-        </NavBarSection>
-        <NavBarSection name="Facilities">
-          <NavBarItem name="Power Facilities" icon="power_facilities.png" />
-          <NavBarItem name="Storage Facilities" icon="storage_facilities.png" />
-          <NavBarItem name="Extraction Facilities" icon="extraction_facilities.png" />
-          <NavBarItem name="Functional Facilities" icon="functional_facilities.png" />
-        </NavBarSection>
-        <NavBarSection name="Community">
-          <NavBarItem name="Messages" icon="messages.png" />
-          <NavBarItem name="Network" icon="network.png" />
-          <NavBarItem name="Map" icon="map.png" />
-          <NavBarItem name="Scoreboard" icon="scoreboard.png" />
-        </NavBarSection>
-        <NavBarItem name="Technology" icon="technology.png" />
-        <NavBarItem name="Resources Market" icon="resource_market.png" />
-      </ul>
+      <div className="tan_green flex-row main_dropdown">
+        <a className="flex-row no_deco">
+          <img className="navbar-logo logo"
+            src="static/images/icon.svg"
+            alt="Energetica logo" />
+          <span className="logo_txt small">Energetica</span>
+        </a>
+        <b onClick={() => (setExpanded(!expanded))}>
+          <span className="navbar-toggler-icon margin"><i className="fa fa-bars" id="dropdown-navbar"></i></span>
+        </b>
+      </div>
+      <div className={`navbar-collapse ${expanded ? 'show' : ''}`} id="menu-navbar">
+        <ul className="navbar-nav">
+          <NavBarItem name="Dashboard" icon="dashboard.png" />
+          <NavBarItem name="Profile" icon="profile.png" />
+          <NavBarSection name="Production Overview">
+            <NavBarItem name="Revenues" icon="revenues.png" />
+            <NavBarItem name="Electricity" icon="power_facilities.png" />
+            <NavBarItem name="Storage" icon="storage_facilities.png" />
+            <NavBarItem name="Resources" icon="resources.png" />
+            <NavBarItem name="Emissions" icon="emissions.png" />
+          </NavBarSection>
+          <NavBarSection name="Facilities">
+            <NavBarItem name="Power Facilities" icon="power_facilities.png" />
+            <NavBarItem name="Storage Facilities" icon="storage_facilities.png" />
+            <NavBarItem name="Extraction Facilities" icon="extraction_facilities.png" />
+            <NavBarItem name="Functional Facilities" icon="functional_facilities.png" />
+          </NavBarSection>
+          <NavBarSection name="Community">
+            <NavBarItem name="Messages" icon="messages.png" />
+            <NavBarItem name="Network" icon="network.png" />
+            <NavBarItem name="Map" icon="map.png" />
+            <NavBarItem name="Scoreboard" icon="scoreboard.png" />
+          </NavBarSection>
+          <NavBarItem name="Technology" icon="technology.png" />
+          <NavBarItem name="Resources Market" icon="resource_market.png" />
+        </ul>
+      </div>
     </nav>
   );
 };
