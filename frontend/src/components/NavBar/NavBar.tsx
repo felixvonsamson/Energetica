f3432545afeddbd02c884e9a8e76bf07f7fc39e4
mglst <maximilien.tirard@gmail.com>
2025-07-23 10:57:31 -0400

    feat: continued styling NavBar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -3,10 +3,10 @@ import { Link } from "@tanstack/react-router";
 import styles from "./NavBar.module.css"
 
 export default function NavBar() {
-    return <nav className={styles.navbar}>
-        <Link to='/admin-dashboard/map'>Map</Link>
-        <Link to='/admin-dashboard/players'>Players</Link>
-        <Link to='/admin-dashboard/networks'>Networks</Link>
-        <Link to='/admin-dashboard/climate'>Climate</Link>
-    </nav>
+    return <nav className={styles['navbar']}>
+        <Link to='/admin-dashboard/map' className={styles['navbar-item']}>Map</Link>
+        <Link to='/admin-dashboard/players' className={styles['navbar-item']}> Players</Link>
+        <Link to='/admin-dashboard/networks' className={styles['navbar-item']}>Networks</Link>
+        <Link to='/admin-dashboard/climate' className={styles['navbar-item']}>Climate</Link>
+    </nav >
 }
