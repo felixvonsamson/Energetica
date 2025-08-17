8d3c258f42f19883d3bcce524985cfcc89ac4a74
mglst <maximilien.tirard@gmail.com>
2025-08-17 18:35:28 +0100

    fix: more precise status code responses

--- tests/integration/test_auth_routes.py
+++ tests/integration/test_auth_routes.py
@@ -22,39 +22,39 @@ def test_signup() -> None:
     # Check login fails on non-existent user
     payload1 = LoginRequest(username="nonexistent_username", password="irrelevant")
     response1 = client.post(login_url, json=payload1.model_dump())
-    assert 400 <= response1.status_code <= 499
+    assert response1.status_code == 401
 
     # Sign up a new user
     payload2 = SignupRequest(username="new_user", password="my-strong-password")
     response2 = client.post(signup_url, json=payload2.model_dump())
-    assert 200 <= response2.status_code <= 299
+    assert response2.status_code == 201
 
     # Log in in new session
     payload3 = LoginRequest(username="new_user", password="my-strong-password")
     response3 = client.post(login_url, json=payload3.model_dump())
-    assert 200 <= response3.status_code <= 299
+    assert response3.status_code == 200
 
     # Log in with wrong password
     payload4 = LoginRequest(username="new_user", password="this-was-my-password-right")
     response4 = client.post(login_url, json=payload4.model_dump())
-    assert 400 <= response4.status_code <= 499
+    assert response4.status_code == 401
 
-    # Try to change password, but
+    # Fail to change password
     payload5 = ChangePasswordRequest(old_password="my-WRONG-password", new_password="my-new-password")
     response5 = client.post(change_password_url, json=payload5.model_dump())
-    assert 400 <= response5.status_code <= 499
+    assert response5.status_code == 400
 
     # Change password, correctly this time
     payload6 = ChangePasswordRequest(old_password="my-strong-password", new_password="my-new-password")
     response6 = client.post(change_password_url, json=payload6.model_dump())
-    assert 200 <= response6.status_code <= 299
+    assert response6.status_code == 204
 
     # Log in with OLD password
     payload7 = LoginRequest(username="new_user", password="my-strong-password")
     response7 = client.post(login_url, json=payload7.model_dump())
-    assert 400 <= response7.status_code <= 499
+    assert response7.status_code == 401
 
     # Log in with new password
     payload8 = LoginRequest(username="new_user", password="my-new-password")
     response8 = client.post(login_url, json=payload8.model_dump())
-    assert 200 <= response8.status_code <= 299
+    assert response8.status_code == 200
