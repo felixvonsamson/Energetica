41d47aca6e5d896a7574255a1d5bdb5d81f88138
mglst <maximilien.tirard@gmail.com>
2025-08-17 18:25:02 +0100

    test: add change-password tests

--- tests/integration/test_auth_routes.py
+++ tests/integration/test_auth_routes.py
@@ -2,7 +2,7 @@
 
 from fastapi.testclient import TestClient
 from energetica import create_app
-from energetica.schemas.auth import LoginRequest, SignupRequest
+from energetica.schemas.auth import ChangePasswordRequest, LoginRequest, SignupRequest
 from energetica.globals import engine
 
 port = 5001
@@ -10,6 +10,7 @@ base_url = f"http://localhost:{port}"
 
 login_url = f"{base_url}/api/v1/auth/login"
 signup_url = f"{base_url}/api/v1/auth/signup"
+change_password_url = f"{base_url}/api/v1/auth/change-password"
 
 
 def test_signup() -> None:
@@ -37,3 +38,23 @@ def test_signup() -> None:
     payload4 = LoginRequest(username="new_user", password="this-was-my-password-right")
     response4 = client.post(login_url, json=payload4.model_dump())
     assert 400 <= response4.status_code <= 499
+
+    # Try to change password, but
+    payload5 = ChangePasswordRequest(old_password="my-WRONG-password", new_password="my-new-password")
+    response5 = client.post(change_password_url, json=payload5.model_dump())
+    assert 400 <= response5.status_code <= 499
+
+    # Change password, correctly this time
+    payload6 = ChangePasswordRequest(old_password="my-strong-password", new_password="my-new-password")
+    response6 = client.post(change_password_url, json=payload6.model_dump())
+    assert 200 <= response6.status_code <= 299
+
+    # Log in with OLD password
+    payload7 = LoginRequest(username="new_user", password="my-strong-password")
+    response7 = client.post(login_url, json=payload7.model_dump())
+    assert 400 <= response7.status_code <= 499
+
+    # Log in with new password
+    payload8 = LoginRequest(username="new_user", password="my-new-password")
+    response8 = client.post(login_url, json=payload8.model_dump())
+    assert 200 <= response8.status_code <= 299
