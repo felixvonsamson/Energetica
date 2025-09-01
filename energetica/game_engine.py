db5170c03184ba737ad38879d8cca90f176a5bc9
Felix <felixvonsamson@gmail.com>
2025-09-01 16:38:14 +0200

    feat: use a random tick value for the delta_t to start at a random season of the year

--- energetica/game_engine.py
+++ energetica/game_engine.py
@@ -119,10 +119,9 @@ class GameEngine(object):
             disable_signups=disable_signups,
         )
         self.log_action(log_entry)
-        last_midnight = self.start_date.replace(hour=0, minute=0, second=0, microsecond=0)
-        # time shift in ticks. Defines the number of ticks between
-        # the first simulated tick and the beginning of in-game year 0.
-        self.delta_t = round((self.start_date - last_midnight).total_seconds() // self.clock_time)
+        # Random time shift in number of ticks to start the game at a random season of the year
+        rng = random.Random(self.random_seed)
+        self.delta_t = rng.randint(0, 72 * 3600 * 24 // self.in_game_seconds_per_tick)
         # transform start_date to a seconds timestamp corresponding to the time of the first tick
         self.start_date = datetime.fromtimestamp(math.floor(self.start_date.timestamp() / clock_time) * clock_time)
 
