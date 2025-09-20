cec48c44050e3e2a09bfd61378a0df0f60cb981a
mglst <maximilien.tirard@gmail.com>
2025-09-20 09:18:54 +0100

    build(react): configure proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,35 @@ export default defineConfig(({ mode }) => ({
         react()
     ],
     base: mode === 'development' ? '/' : '/static/react/',
+    server: {
+        proxy: {
+            '^/landing$': {
+                target: 'http://localhost:5001/landing',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/landing/, '')
+            },
+            '/sign-up': {
+                target: 'http://localhost:5001/sign-up',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/sign-up/, '')
+            },
+            '/login': {
+                target: 'http://localhost:5001/login',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/login/, '')
+            },
+            '/static': {
+                target: 'http://localhost:5001/static',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/static/, '')
+            },
+            '/api': {
+                target: 'http://localhost:5001/api',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    },
     build: {
         outDir: '../energetica/static/react',
         emptyOutDir: true
