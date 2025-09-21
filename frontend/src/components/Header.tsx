887686015c1a63c138c28683d185de8028a60e6d
mglst <maximilien.tirard@gmail.com>
2025-09-21 23:05:46 +0100

    feat(frontend): updated hamburger logo to lucide icon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@tanstack/react-router";
+import { Menu } from "lucide-react";
 
 export function Header() {
     return (
@@ -23,21 +24,7 @@ export function Header() {
                         className="-m-2.5 inline-flex items-center justify-center rounded-4xl p-2.5 text-gray-800"
                     >
                         <span className="sr-only">Open main menu</span>
-                        <svg
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="1.5"
-                            data-slot="icon"
-                            aria-hidden="true"
-                            className="size-6"
-                        >
-                            <path
-                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                            />
-                        </svg>
+                        <Menu />
                     </button>
                 </div>
                 <header>
