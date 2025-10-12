3f88ec1f3212a71d7212f5a66f0b5ed75ecd7879
mglst <maximilien.tirard@gmail.com>
2025-10-12 14:11:25 +0100

    fix(frontend): removed half-implemented hamburger menu

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
                 aria-label="Global"
                 className="flex items-center justify-between px-6 py-8 lg:px-8"
             >
-                <div className="flex lg:flex-1">
+                <div className="flex flex-1">
                     <Link
                         to="/landing-page"
                         className="-m-1.5 p-1.5 flex items-center"
@@ -18,17 +18,8 @@ export function Header() {
                         </span>
                     </Link>
                 </div>
-                <div className="flex lg:hidden">
-                    <button
-                        type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-4xl p-2.5 text-gray-800"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <Menu />
-                    </button>
-                </div>
                 <header>
-                    <div className="hidden lg:flex lg:gap-x-8">
+                    <div className="flex gap-x-8">
                         <Link
                             to={"/landing-page"}
                             className="text-md font-semibold text-gray-800 hover:text-gray-700"
@@ -49,7 +40,7 @@ export function Header() {
                         </Link>
                     </div>
                 </header>
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <div className="flex flex-1 justify-end">
                     <a
                         href="/login"
                         className="bg-gray-800 text-light-green px-6 py-2 rounded-4xl text-sm font-semibold hover:bg-gray-700 shadow-md hover:shadow-lg"
