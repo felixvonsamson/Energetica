0af1e1ef893072e6483f70cbc69acd499e3a04fc
mglst <maximilien.tirard@gmail.com>
2026-03-24 22:59:15 +0000

    refactor: abstract priority bump logic

--- energetica/routers/power_priorities.py
+++ energetica/routers/power_priorities.py
@@ -46,25 +46,7 @@ def increase_priority(
 ) -> PowerPrioritiesListOut:
     if not player.achievements["network"]:
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail=UNLOCK_NETWORK_MESSAGE)
-
-    priorities = player.network_prices.get_facility_priorities(player)
-
-    idx = next(
-        (i for i, p in enumerate(priorities) if p.side == side and p.type.value == item_type),
-        None,
-    )
-    if idx is None:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"Item {side}/{item_type} not found")
-
-    if idx == 0:
-        return PowerPrioritiesListOut.from_player(player)
-
-    reordered = list(priorities)
-    reordered[idx], reordered[idx - 1] = reordered[idx - 1], reordered[idx]
-
-    player.network_prices.change_facility_priority(player, reordered)
-    player.invalidate_queries(["power-priorities"], ["facilities"])
-
+    player.network_prices.shift_facility_priority(player, side, item_type, -1)
     return PowerPrioritiesListOut.from_player(player)
 
 
@@ -76,23 +58,5 @@ def decrease_priority(
 ) -> PowerPrioritiesListOut:
     if not player.achievements["network"]:
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail=UNLOCK_NETWORK_MESSAGE)
-
-    priorities = player.network_prices.get_facility_priorities(player)
-
-    idx = next(
-        (i for i, p in enumerate(priorities) if p.side == side and p.type.value == item_type),
-        None,
-    )
-    if idx is None:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"Item {side}/{item_type} not found")
-
-    if idx == len(priorities) - 1:
-        return PowerPrioritiesListOut.from_player(player)
-
-    reordered = list(priorities)
-    reordered[idx], reordered[idx + 1] = reordered[idx + 1], reordered[idx]
-
-    player.network_prices.change_facility_priority(player, reordered)
-    player.invalidate_queries(["power-priorities"], ["facilities"])
-
+    player.network_prices.shift_facility_priority(player, side, item_type, 1)
     return PowerPrioritiesListOut.from_player(player)
